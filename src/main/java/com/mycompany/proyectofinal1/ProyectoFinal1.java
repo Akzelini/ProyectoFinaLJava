346091c35834a074ffaf3a875d8b0fd586d9b247
Akzelini <distopicorizaceo@gmail.com>
2026-04-23 20:12:49 -0700

    ProyectoFinal Akzelini Astrid y Rey David

---
+++ src/main/java/com/mycompany/proyectofinal1/ProyectoFinal1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyectofinal1;
+
+/**
+ *
+ * @author JC
+ */
+public class ProyectoFinal1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
