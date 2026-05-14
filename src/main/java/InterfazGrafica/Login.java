0ca2b05e9cf3c30378f711e0a95d4957a22af21b
Akzelini <distopicorizaceo@gmail.com>
2026-05-13 17:28:48 -0700

    holaaaaa

--- src/main/java/InterfazGrafica/Login.java
+++ src/main/java/InterfazGrafica/Login.java
@@ -2,6 +2,9 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
  */
+
+
+// akzelini
 package InterfazGrafica;
 
 import javax.swing.*;
@@ -12,6 +15,7 @@ import javax.swing.JOptionPane;
 import InterfazGrafica.VentanaAnimales;
 import modelo.Cultivos;
 import InterfazGrafica.IgInventario;
+import  InterfazGrafica.IgInventario;
 
 
 /**
